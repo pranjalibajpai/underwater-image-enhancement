021d1607f0a1b72f167bff31e620ef7722458711
Pranjali Bajpai <2018meb1243@iitrpr.ac.in>
2021-12-19 19:56:46 +0530

    minor update

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
-mkdir -p ~/.streamlit/                                               
-echo "\                       
-[server]\n\                       
-port = $PORT\n\                       
-enableCORS = false\n\                       
-headless = true\n\                       
-\n\                       
+mkdir -p ~/.streamlit/
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
